1105b73667bf236bca045650b3408e0b960d585d
Norberto Caceres <norbe1994@yahoo.com>
2019-04-01 14:37:39 -0500

    agregar play button widget de spotify en pagina de artista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,17 +9,28 @@ import { HomeComponent } from './components/home/home.component'
 import { SearchComponent } from './components/search/search.component'
 import { ArtistaComponent } from './components/artista/artista.component'
 import { NavbarComponent } from './components/shared/navbar/navbar.component'
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component'
+import { LoadingComponent } from './components/shared/loading/loading.component'
 // importar rutas
 import { ROUTES } from './app.routes'
 // importar servicios
 
 // importar pipes
-import { NoimagePipe } from './pipes/noimage.pipe';
-import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
-import { LoadingComponent } from './components/shared/loading/loading.component'
+import { NoimagePipe } from './pipes/noimage.pipe'
+import { DomseguroPipe } from './pipes/domseguro.pipe'
 
 @NgModule({
-	declarations: [AppComponent, HomeComponent, SearchComponent, ArtistaComponent, NavbarComponent, NoimagePipe, TarjetasComponent, LoadingComponent],
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		SearchComponent,
+		ArtistaComponent,
+		NavbarComponent,
+		NoimagePipe,
+		DomseguroPipe,
+		TarjetasComponent,
+		LoadingComponent,
+	],
 	imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(ROUTES, { useHash: true })],
 	providers: [],
 	bootstrap: [AppComponent],
